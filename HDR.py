b2e4fa6f7c80bcf83e513c9a2fa53b6ef56c1cf2
balakrishna-k <32950521+balakrishna-k@users.noreply.github.com>
2018-11-15 00:49:39 -0800

    Completed Project
    * fixed Histograms
    
    * Fixed Histograms title spacing
    
    * made small changes to HDR
    
    * finished
    
    * Final, Finished Code

--- HDR.py
+++ HDR.py
@@ -8,34 +8,42 @@ from hdr import composition as cp
 
 from utility import constants as ct
 
-DISPLAY = ct.DONT_DISPLAY_PLOT
+DISPLAY = ct.DISPLAY_PLOT
+composite_images = []
 
+# ------------------------------------------------------------------
+# Learn Gamma Parameters for Camera
 g = gc.learn_gamma_parameters_and_plot(display=DISPLAY)
 
-print(g)
 
-images = xp.generate_hdr_stack_histogram(file_name="Histograms.png", file_name_exp="Histograms-exp.png", gamma_params=g, display=DISPLAY)
+# ------------------------------------------------------------------
+# Generate Histograms for Picture Stack
+images = xp.generate_hdr_stack_histogram(file_name="Histograms.png", file_name_exp="Histograms-exp.png",
+                                         gamma_params=g, display=DISPLAY)
 
+
+# ------------------------------------------------------------------
+# Get Composite by Average
 HDR = cp.average_pixel_hdr(g, images, ct.EXPOSURE_TIMES)
-
+composite_images.append(HDR)
 HDR = np.float32(HDR / 255)
-
+# Using Reinhard Tone-map
 tonemapReinhard = cv2.createTonemapReinhard(0.8, 0, 0, 0)
 ldrReinhard = tonemapReinhard.process(HDR)
-cv2.imwrite("ldr-average.jpg", ldrReinhard * 255)
+cv2.imwrite(ct.HDR_WRITE_PATH + "/HDR-average.jpg", ldrReinhard * 255)
 
-HDR = cp.average_pixel_hdr(g, images, ct.EXPOSURE_TIMES)
 
-HDR = np.float32(HDR / 255)
-
-tonemapReinhard = cv2.createTonemapReinhard(0.8, 0, 0, 0)
-ldrReinhard = tonemapReinhard.process(HDR)
-cv2.imwrite("ldr-average.jpg", ldrReinhard * 255)
-
+# ------------------------------------------------------------------
+# Get Composite by Pixel value/saturation
 HDR = cp.best_pixel_hdr(g, images, ct.EXPOSURE_TIMES)
-
+composite_images.append(HDR)
 HDR = np.float32(HDR / 255)
-
+# Using Reinhard Tone-map
 tonemapReinhard = cv2.createTonemapReinhard(0.8, 0, 0, 0)
 ldrReinhard = tonemapReinhard.process(HDR)
-cv2.imwrite("ldr-best.jpg", ldrReinhard * 255)
+cv2.imwrite(ct.HDR_WRITE_PATH + "/HDR-best.jpg", ldrReinhard * 255)
+
+
+# ------------------------------------------------------------------
+# Generate Histograms for Composite
+xp.__generate_composite_histogram__(composite_images, display=DISPLAY)
--- hdr/exposure.py
+++ hdr/exposure.py
@@ -1,11 +1,14 @@
 import numpy as np
 
+import cv2 as cv
+
 from matplotlib import pyplot as plt
 
 from hdr import gamma as gc
 
 from utility import imageutil as im
 from utility import constants as ct
+
 
 plt.rc('font', size=ct.SMALL_SIZE)          # controls default text sizes
 plt.rc('axes', titlesize=ct.SMALL_SIZE)     # fontsize of the axes title
@@ -39,7 +42,9 @@ def generate_hdr_stack_histogram(file_name, file_name_exp, gamma_params, display
 
 def __generate_histogram__(file_name, images, gamma_params, display=ct.DONT_DISPLAY_PLOT):
     graph_num = 0
+    plt.figure(4)
     for i, image in enumerate(images):
+        # Histogram needs Float32 values, which is a callback function
         img = gc.invert_gamma_of_image(image, gamma_params, np.float32)
         for j in range(0, 3):
             graph_num = graph_num + 1
@@ -48,7 +53,9 @@ def __generate_histogram__(file_name, images, gamma_params, display=ct.DONT_DISP
             plt.plot(hist, color=ct.CHANNEL_COLOUR[j])
             plt.title(ct.CHANNEL[j] + " Channel  \nPicture " + str(i + 1))
 
+    plt.suptitle("HDR Stack Histogram")
     plt.tight_layout()
+    plt.subplots_adjust(top=0.8)
     plt.savefig(ct.EXPOSURE_WRITE_PATH + "/" + file_name)
 
     if display is True:
@@ -56,11 +63,13 @@ def __generate_histogram__(file_name, images, gamma_params, display=ct.DONT_DISP
 
 
 def __generate_exp_histogram__(file_name, images, gamma_params, exp_times, display=ct.DONT_DISPLAY_PLOT):
+    # Histogram needs Float32 values, which is a callback function
     img1 = (gc.invert_gamma_of_image(images[1], gamma_params, np.float32))/exp_times[1]
     img2 = (gc.invert_gamma_of_image(images[2], gamma_params, np.float32))/exp_times[2]
 
     img = [img1, img2]
     graph_num = 0
+    plt.figure(5)
     for i, image in enumerate(img):
         for j in range(0,3):
             graph_num = graph_num + 1
@@ -69,8 +78,31 @@ def __generate_exp_histogram__(file_name, images, gamma_params, exp_times, displ
             plt.plot(hist, color=ct.CHANNEL_COLOUR[j])
             plt.title(ct.CHANNEL[j] + " Channel \nPicture " + str(i + 1))
 
+    plt.suptitle("Histograms for B'(a1*T)/a1 and B'(a2*T)/a2")
     plt.tight_layout()
+    plt.subplots_adjust(top=0.8)
     plt.savefig(ct.EXPOSURE_WRITE_PATH + "/" + file_name)
+
+    if display is True:
+        plt.show()
+
+
+def __generate_composite_histogram__(images, display=ct.DONT_DISPLAY_PLOT):
+    graph_num = 0
+    plt.figure(6)
+    for i, image in enumerate(images):
+        image = np.float32(image)
+        for j in range(0,3):
+            graph_num = graph_num + 1
+            plt.subplot(int("23" + str(graph_num)))
+            hist = cv.calcHist([image], [j], None, [256], [0, 255])
+            plt.plot(hist, color=ct.CHANNEL_COLOUR[j])
+            plt.title(ct.CHANNEL[j] + "Composite Channel")
+
+    plt.suptitle("Histograms for Composite Image")
+    plt.tight_layout()
+    plt.subplots_adjust(top=0.8)
+    plt.savefig(ct.HDR_WRITE_PATH + "/CompositeHistogram.png")
 
     if display is True:
         plt.show()
